2a99951e234e72b7a8d118becbd9e5a6d2592d68
Avinash Kumar <avinash.k95347@gmail.com>
2022-07-20 12:26:25 +0530

    second commit : renamed pattern5.java due to build errors added a file interface.java

--- JAVA OOPS/Interfaces.java
+++ JAVA OOPS/Interfaces.java
@@ -1,3 +1,18 @@
+interface Car {
+    public void start();
+}
+
+class Maruti implements Car {
+
+    @Override
+    public void start() {
+        System.out.println("Maruti starts bhroom bhroom ...");
+    }
+}
+
 public class Interfaces {
-    
+    public static void main(String[] args) {
+        Maruti obj1 = new Maruti();
+        obj1.start();
+    }
 }
